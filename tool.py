e6286e6e83ba9022349f3d63c0ee8877b7dfaba8
Piero Antonio Zecca <pieroantonio@users.noreply.github.com>
2024-05-17 11:26:33 +0200

    tool

--- tool.py
+++ tool.py
@@ -13,15 +13,14 @@ def carica_modello(nome_file):
     return load(nome_file)
 
 class PrevisioniBoxLayout(BoxLayout):
-
-    def __init__(self, input_features, output_features, **kwargs):
+    def __init__(self, initial_input_features, prediction_sequence, **kwargs):
         super().__init__(orientation="vertical", **kwargs)
-        self.modello = {feature: carica_modello(f'modello_migliore_{feature}.joblib') for feature in output_features}
-        self.input_features = input_features
-        self.output_features = output_features
+        self.initial_input_features = initial_input_features
+        self.prediction_sequence = prediction_sequence
         self.inputs = {}
-        
-        for feature in self.input_features:
+        self.models = {feature: carica_modello(f'modello_{feature}.joblib') for feature in self.prediction_sequence}
+
+        for feature in self.initial_input_features:
             row = BoxLayout(orientation="horizontal", size_hint_y=None, height=30)
             row.add_widget(Label(text=f"{feature}: ", size_hint_x=None, width=100))
             input_box = TextInput(multiline=False)
@@ -33,13 +32,16 @@ class PrevisioniBoxLayout(BoxLayout):
         predict_button.bind(on_press=self.on_predici_button_clicked)
         self.add_widget(predict_button)
 
+        reset_button = Button(text="Reset", size_hint_y=None, height=50)
+        reset_button.bind(on_press=self.on_reset_button_clicked)
+        self.add_widget(reset_button)
+
     def on_predici_button_clicked(self, instance):
         try:
-            input_values = [float(self.inputs[feature].text) for feature in self.input_features]
-            previsioni = self.effettua_predizione(input_values)
-            # Arrotonda le previsioni al primo decimale e crea una stringa per il Popup
-            previsioni_str = "\n".join([f"{k}: {np.round(v, 1)}" for k, v in previsioni.items()])
-            scroll_content = Label(text=previsioni_str, size_hint_y=None)
+            input_data = {feature: float(self.inputs[feature].text) if self.inputs[feature].text else np.nan for feature in self.initial_input_features}
+            predictions = self.effettua_predizione_in_catena(input_data)
+            predictions_str = "\n".join([f"{k}: {np.round(v, 2)}" for k, v in predictions.items() if pd.notnull(v)])
+            scroll_content = Label(text=predictions_str, size_hint_y=None)
             scroll_content.bind(texture_size=scroll_content.setter('size'))
             content = ScrollView(size_hint=(None, None), size=(400, 400))
             content.add_widget(scroll_content)
@@ -49,19 +51,25 @@ class PrevisioniBoxLayout(BoxLayout):
             popup = Popup(title="Errore", content=Label(text="Inserire valori numerici validi."), size_hint=(None, None), size=(400, 200))
             popup.open()
 
-    def effettua_predizione(self, input_values):
-        dati_nuovi_df = pd.DataFrame([input_values], columns=self.input_features)
-        previsioni = {}
-        for feature, model in self.modello.items():
-            previsione = model.predict(dati_nuovi_df)[0]
-            previsioni[feature] = previsione
-        return previsioni
+    def effettua_predizione_in_catena(self, input_data):
+        input_df = pd.DataFrame([input_data])
+        for feature in self.prediction_sequence:
+            if pd.isnull(input_data.get(feature)):
+                model = self.models[feature]
+                prediction = model.predict(input_df)[0]
+                input_data[feature] = prediction
+                input_df.at[0, feature] = prediction
+        return input_data
+
+    def on_reset_button_clicked(self, instance):
+        for input_box in self.inputs.values():
+            input_box.text = ''  # Clear the text in each input box
 
 class PrevisioniApp(App):
     def build(self):
-        input_features = ['Sex', 'Age', 'WITSA', 'OB_FH', 'OJ_OC', 'U6L6D', 'LFHNP', 'IIANG', 'IMPA']
-        output_features = ['SNA', 'SNB', 'ANB', 'NP2PA', 'NP2PO', 'SNDST', 'SNFHA', 'CONVX', 'AB2FH', 'TFHNP', 'PLHNP', 'U1SNA', 'FMPA', 'SADLA', 'COPAD', 'COPOD']
-        return PrevisioniBoxLayout(input_features, output_features)
+        initial_input_features = ['Sex', 'Age', 'WITSA', 'OB_FH', 'OJ_OC', 'U6L6D', 'LFHNP', 'IIANG', 'IMPA']
+        prediction_sequence = ['PLHNP', 'SNB', 'SNA', 'CONVX', 'COPOD', 'SADLA', 'SNFHA', 'SNDST', 'COPAD', 'TFHNP', 'FMPA', 'NP2PA', 'ANB', 'U1SNA', 'NP2PO', 'AB2FH']
+        return PrevisioniBoxLayout(initial_input_features, prediction_sequence)
 
 if __name__ == "__main__":
     PrevisioniApp().run()
